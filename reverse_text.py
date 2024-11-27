92c15a19f0a5c3ddf7c1b975dd8e899c6d3796f7
Kazuya Ochiai <s23C1033WD@s.chibakoudai.jp>
2024-11-27 12:07:34 +0900

    add a command

---
+++ reverse_text.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2024 Kazuya Ochiai
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+import sys
+
+#標準入力からデータを読み込む
+for line in sys.stdin:
+	reversed_line = line.strip()[::-1]
+	print(reversed_line)
+
